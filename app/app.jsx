bb685dda7d118be989d964cf3a0bd7edd6f5c130
Chuck Verret <chuck@chuckverret.com>
2017-03-28 23:12:26 -0500

    refactor to work with local storage

--- app/app.jsx
+++ app/app.jsx
@@ -4,13 +4,18 @@ var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
-
 var actions = require('actions');
 var store = require('configureStore').configure();
+var TodoApi = require('TodoApi');
 
 store.subscribe(() => {
-  console.log('New state', store.getState());
+  var state = store.getState();
+  console.log('New state', state);
+  TodoApi.setTodos(state.todos);
 });
+
+var initialTodos = TodoApi.getTodos();
+store.dispatch(actions.addTodos(initialTodos));
 
 //Load foundation
 
